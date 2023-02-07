4287e37a7aef082356afbc5c0687b90bc61cba5d
zaheer hanif <za00901@gmail.com>
2023-02-07 14:37:32 +0500

    some modification

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,4 +3,4 @@ set :repo_url, "git@github.com:zaheerabbas0/demoProject.git"
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 set :rvm_ruby_version, '2.5.7'
 set :passenger_restart_with_touch, true
-# server "35.79.52.24",  roles: [:app, :web, :db], :primary => true 
+server "35.79.52.24", user: 'ubuntu',port: "5432", roles: [:app, :web, :db], :primary => true 
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-server '35.79.52.24', user: 'ubuntu', roles: %w{app db web}
+server '35.79.52.24', user: 'ubuntu',port: "5432", roles: %w{app db web}
 set :ssh_options, {
    keys: %w(/home/zaheer/.ssh/id_rsa),
    forward_agent: false,
